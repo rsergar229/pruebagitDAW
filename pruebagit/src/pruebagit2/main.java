53a4adc2b64573c869eb12a6d3985dbf042e99a2
roberto <roberto@localhost>
2023-03-28 18:41:41 +0200

    Nuevo DAW

--- pruebagit/src/pruebagit2/main.java
+++ pruebagit/src/pruebagit2/main.java
@@ -4,7 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola buenos dias");
+		System.out.println("Hola buenos dias tenga usted");
+		System.out.println("ADios");
 	}
 
 }
