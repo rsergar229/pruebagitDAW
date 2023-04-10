e8b7ef532e81e4e6ec59c0154b49a8844931ece0
roberto <roberto@localhost>
2023-04-10 16:29:12 +0200

    Probando DAW1

--- pruebagit/src/pruebagit2/main.java
+++ pruebagit/src/pruebagit2/main.java
@@ -6,6 +6,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("Hola buenos dias tenga usted");
 		System.out.println("ADios");
+		System.out.println("Hola");
 	}
 
 }
